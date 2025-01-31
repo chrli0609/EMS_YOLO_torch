db02ddb9c91b2fd1effcefe53e0e1ee9639d7422
chris <christopher.liu.0609@gmail.com>
2025-01-31 23:39:28 +0100

    debugging device id

--- GEN1_EMS_YOLO_LU/utils/torch_utils.py
+++ GEN1_EMS_YOLO_LU/utils/torch_utils.py
@@ -72,6 +72,7 @@ def select_device(device='', batch_size=None, newline=True):
             assert batch_size % n == 0, f'batch-size {batch_size} not multiple of GPU count {n}'
         space = ' ' * (len(s) + 1)
         for i, d in enumerate(devices):
+            print("i", i)
             p = torch.cuda.get_device_properties(i)
             s += f"{'' if i == 0 else space}CUDA:{d} ({p.name}, {p.total_memory / 1024 ** 2:.0f}MiB)\n"  # bytes to MB
     else:
